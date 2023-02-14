1a5d52ea1c5ac2bced39ff944f30a1273c5c763c
Matthias Behr <mbehr@mcbehr.de>
2023-02-14 06:43:55 +0100

    wip: use local json editor

--- src/components/RQEditor.tsx
+++ src/components/RQEditor.tsx
@@ -6,39 +6,58 @@ import { withHistory } from 'slate-history';
 
 import React from "react";
 
+import { Editor as JSONEditor } from "json-editor"
+import { TSJSONValue } from "json-editor/dist/Editor";
+
 export type RQValue = string;
+
+/**
+ * // parse as regex: ^\/(?<cmd>.*?)\/(?<path>.*?)($|\?(?<options>.+)$)
+        var re = /^\/(?<cmd>.*?)\/(?<path>.*?)($|\?(?<options>.+)$)/;
+        // options are splitted/separated by & but then decodeURIComponent... on part after =
+ * example:
+ ext:mbehr1.dlt-logs/get/docs/0/filters?
+delete={
+  "tmpFb": 1
+}
+ &
+disableAll=view &
+add={
+  "lifecycles": "${attributes.lifecycles.id}",
+  "name": "not selected lifecycles",
+  "not": true,
+  "tmpFb": 1,
+  "type": 1
+}
+ */
+
+type RQText = {
+    children: { text: string }[],
+}
 
 type RQJson = {
     type: 'RQJson',
+    jsonValue: TSJSONValue,
     children: ({ text: string })[]
 }
 
 type RQDoc = {
     type: 'RQDoc',
-    children: (RQJson)[]
+    children: (RQText | RQJson)[]
 }
 
 
 declare module 'slate' {
     interface CustomTypes {
         Editor: BaseEditor & ReactEditor
-        Element: RQDoc | RQJson,
+        Element: RQDoc | RQJson | RQText,
         Text: { text: string }//, type?: string }
     }
 }
 
-const withRQElements = (editor: ReactEditor) => {
-    const { normalizeNode } = editor;
-    editor.normalizeNode = ([node, path]) => {
-        return normalizeNode([node, path])
-    }
-
-    return editor
-}
-
 const deserialize = (obj: RQValue | undefined): Descendant[] => {
     console.log(`rq.deserialize(${JSON.stringify(obj)})...`)
-    return [{ type: 'RQDoc', children: [{ type: 'RQJson', children: [{ text: "" }] }] }]
+    return [{ type: 'RQDoc', children: [{ children: [{ text: 'ext:mbehr1.dlt-logs/get/docs/0/filters?delete=' }] }, { type: 'RQJson', jsonValue: { 'foo': true }, children: [{ text: '' }/* needed for selection*/] }] }]
 }
 
 export const serialize = (value: Descendant[]): RQValue | undefined => {
@@ -52,14 +71,14 @@ const debugObj = (v: Descendant | Descendant[], indent: number): string => {
         return indentOffset + v.map(e => debugObj(e, indent)).join('\n' + indentOffset);
     }
     if (typeof (v) === 'object' && 'children' in v) {
-        const isValid = v.type === 'RQJson' ? true : false; // todo
-        const type = v.type;
+        const isValid = 'type' in v && v.type !== 'RQDoc' ? true : false; // todo
+        const type = 'type' in v ? v.type : undefined;
         return (isValid ? '+' : '-') + indentOffset + `${type}:[(#children=${v.children.length})\n` + debugObj(v.children, indent + 1);
     }
     if (typeof (v) === 'object' && 'type' in v) {
         const isValid = true;
         const type = v.type;
-        return (isValid ? '+' : '-') + indentOffset + `${type}: '${v.text}'`;
+        return (isValid ? '+' : '-') + indentOffset + `${type}: '${'text' in v ? v.text : JSON.stringify(v)}'`;
     }
     if (typeof (v) === 'object' && 'text' in v) {
         return indentOffset + `text: '${v.text}'`;
@@ -72,6 +91,18 @@ const debugHtml = (value: Descendant[]) => {
     return debugObj('children' in value[0] ? value[0].children : value, 0);
 }
 
+const withRQElements = (editor: ReactEditor) => {
+    const { normalizeNode, isVoid, isInline } = editor;
+    editor.normalizeNode = ([node, path]) => {
+        return normalizeNode([node, path])
+    }
+
+    editor.isInline = element => ('type' in element && element.type === 'RQJson' ? false : isInline(element))
+    editor.isVoid = element => ('type' in element && element.type === 'RQJson' ? true : isVoid(element)) // no text
+
+    return editor
+}
+
 export default function RQEditor({ object, getEditor }: { object: RQValue | undefined, onChange?: ((v: RQValue | undefined) => void), getEditor?: (ed: ReactEditor) => void }) {
     const editor = useMemo(() => withRQElements(withHistory(withReact(createEditor()))), []);
     const [document, setDocument] = useState(deserialize(object));
@@ -79,14 +110,20 @@ export default function RQEditor({ object, getEditor }: { object: RQValue | unde
     if (getEditor) { getEditor(editor); }
 
     const renderElement = useCallback((props: RenderElementProps) => {
+        if ('type' in props.element) {
         switch (props.element.type) {
-            case 'RQJson':
+            case 'RQJson': {
                 //console.log(`renderElement JsonObject called`, props.element.children[0].children[0]);
                 // <div contentEditable={false} style={{ userSelect: "none" }}>{'}'}</div>
-                return (<pre style={{ backgroundColor: 'lightgrey' }}{...props.attributes}><code>{props.children}</code></pre >);
+                const je = JSONEditor.default({ object: props.element.jsonValue });
+                return <div {...props.attributes}><div contentEditable={false}>{je}</div>{props.children}</div>;
+            }
             default:
                 //console.log(`renderElement ${props.element.type}...`);
                 return <DefaultElement {...props} />
+        }
+        } else {
+            return <DefaultElement {...props} />
         }
     }, []);
 
